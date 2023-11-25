17e0923e60efffab1e2c812165d87cd8845fcc12
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-11-24 17:43:27 -0800

    add car card

---
+++ components/CarDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface CarDetailsProps{
+    isOpen: boolean;
+    closeModal: () => void;
+}
+
+const CarDetails = () => {
+  return (
+    <div>CarDetails</div>
+  )
+}
+
+export default CarDetails
